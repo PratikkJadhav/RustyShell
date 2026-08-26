0025e25702868b99546c6cc1bfdca5ead6396c5e
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-08-26 16:34:04 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    while input != "x" {
+        input.clear();
+        io::stddin().read_line(&mut input).unwrap();
+        println!("{}",input," Not found");
+    }
+
 }

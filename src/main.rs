bfa47dbaee641484749313a66c39eac3df94ea8a
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-08-26 17:01:19 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,22 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
 
-    let mut input = String::new();
-
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        input.clear();
-        io::stdin().read_line(&mut input).unwrap();
-        command = input.trim().to_string();
-                if command == "exit" {
-                    break;
-                } else if command.starts_with("echo ") {
-                    println!("{}", &command[5..]);
-                } else {
-                    println!("{}: command not found", command);
-                }
+            print!("$ ");
+            io::stdout().flush().unwrap();
+            // Wait for user input
+            let mut command = String::new();
+            io::stdin().read_line(&mut command).unwrap();
+            command = command.trim().to_string();
+            if command == "exit" {
+                break;
+            } else if command.starts_with("echo ") {
+                println!("{}", &command[5..]);
+            } else {
+                println!("{}: command not found", command);
+            }
+        }
 
-    }
 
 
 }

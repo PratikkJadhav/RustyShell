107d80537b5a8e134dd83ef04069b2cf7be8a8a9
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-08-26 16:40:51 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
 
 
-    let mut input = String::new();
+
     while true {
         print!("$ ");
         io::stdout().flush().unwrap();
+        let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         println!("{}: command not found", input.trim())
     }

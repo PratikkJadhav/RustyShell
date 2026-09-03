c4a0aed01ae574298af77d483e081804d4078313
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-09-03 16:48:32 +0000

    locate exec files

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{env, fs, os::unix::fs::PermissionsExt, path::Path};
 
 fn main() {
     loop {
-            print!("$ ");
-            io::stdout().flush().unwrap();
-            let mut command = String::new();
-            io::stdin().read_line(&mut command).unwrap();
-            command = command.trim().to_string();
-            if command == "exit" {
-                break;
-            } else if command.starts_with("echo ") {
-                println!("{}", &command[5..]);
-            } else if command.starts_with("type ") {
-                let c = &command[5..];
-                if c == "echo" || c == "exit" || c=="type"{
-                    println!("{} is a shell builtin", c );
-                }else{
-                    println!("{}: not found", c );
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        command = command.trim().to_string();
+        let mut parse = command.splitn(2, char::is_whitespace);
+        let cmd = parse.next().unwrap();
+        let args = parse.next().unwrap_or("").trim_start();
+        if cmd == "exit" {
+            break;
+        } else if cmd == "echo" {
+            println!("{args}");
+        } else if cmd == "type" {
+            if args == "echo" || args == "exit" || args == "type" {
+                println!("{} is a shell builtin", args);
+            } else {
+                exec(args);
+            }
+        } else {
+            println!("{}: command not found", cmd);
+        }
+    }
+}
+
+fn exec(args: &str) {
+    let path = env::var("PATH").unwrap();
+    let paths: Vec<&str> = path.split(":").collect();
+    for dir in paths {
+        let file = Path::new(dir).join(args);
+        if file.is_file() {
+            if let Ok(metadata) = fs::metadata(&file) {
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    println!("{} is {}", args, file.to_str().unwrap());
+                    return;
                 }
-            }else {
-                println!("{}: command not found", command);
             }
         }
+    }
 
-
-
+    println!("{}: not found", args);
 }
